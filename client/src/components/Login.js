675b41152b7875e9d978f08c34e58ebef84ee368
Randy <rlthomasccv@gmail.com>
2019-06-23 18:15:00 -0700

    Finalize Login with token auth

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,20 +1,42 @@
-import React from 'react';
+import React, {Component} from 'react';
 import axios from 'axios';
 import { Redirect, Route, HashRouter, Link } from 'react-router-dom';
+const ls = require('../../utils/storage');
 
 
-const Login = (props) => {
 
-    const handleSubmit = (e) => {
-        console.log(props)
+
+class Login extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    componentDidMount() {
+        const token = ls.getFromStorage('token');
+        console.log(token, 'token initial login')
+        axios.get('/session', {token})
+        .then((data) => {
+            if (data.data.success === true) {
+                this.props.toggleLog();
+            } else {
+                console.log('Token is invalid')
+            }
+        })
+        .catch((err) => console.log(err));
+    }
+
+
+    handleSubmit(e) {
+        console.log(this.props)
         e.preventDefault();
         const user = e.target.username.value;
         const pass = e.target.password.value;
         axios.post('/login', {user, pass})
         .then((data) => {
             if (data.data.success === true) {
-                props.setToken(data.data.token)
-                props.toggleLog()
+                this.props.setToken(data.data.token)
+                this.props.toggleLog()
+                ls.setInStorage(data.data.token)
             } 
             if (data.data.success === false) {
                 alert(data.data.message)
@@ -23,26 +45,32 @@ const Login = (props) => {
         .catch((err) => console.log(err));
     }
 
-    const {isLoggedIn} = props
-    if (!isLoggedIn) {
-        return (
-            <div className="container">
-                <form onSubmit={handleSubmit}>
-                    <div className="form-group">
-                        <label>Username</label>
-                        <input type="text" id="username" className="form-control" placeholder="Enter Username" />
-                    </div>
-                    <div className="form-group">
-                        <label>Password</label>
-                        <input type="password" id="password" className="form-control" placeholder="Enter Password" />
-                    </div>
-                    <input type="submit" className="btn btn-primary" value="Submit" />
-                </form>
-            </div>
-        )
+
+
+    render() { 
+        const {isLoggedIn} = this.props
+        if (!isLoggedIn) {
+            return (
+                <div className="container">
+                    <form onSubmit={this.handleSubmit.bind(this)}>
+                        <div className="form-group">
+                            <label>Username</label>
+                            <input type="text" id="username" className="form-control" placeholder="Enter Username" />
+                        </div>
+                        <div className="form-group">
+                            <label>Password</label>
+                            <input type="password" id="password" className="form-control" placeholder="Enter Password" />
+                        </div>
+                        <input type="submit" className="btn btn-primary" value="Submit" />
+                    </form>
+                </div>
+            )
+        }
+        return <Redirect to="/home" />
     }
-    return <Redirect to="/home" />
 }
+ 
+
 
 
 export default Login
