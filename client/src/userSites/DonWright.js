a0d2953a1f0abd1aa6eccbfd336af3d1d764f115
Randy <rlthomasccv@gmail.com>
2019-07-04 16:36:34 -0700

    Added Tables to User Pages

--- client/src/userSites/DonWright.js
+++ client/src/userSites/DonWright.js
@@ -1,3 +1,4 @@
+/* eslint-disable class-methods-use-this */
 /* eslint-disable react/jsx-filename-extension */
 /* eslint-disable no-unused-vars */
 import React, { Component } from 'react';
@@ -5,23 +6,116 @@ import Navbar from '../components/Navbar';
 import ActionButtons from '../components/ActionButtons';
 import DonChart from '../charts/DonChart';
 
-const DonWright = (props) => {
-  const navLinks = () => (
-    <div className="text-center" id="userLinkContainer">
-      <a href="#/don10Year" id="userLinks">10 Year Plan</a>
-      <a href="#/don5Year" id="userLinks">5 Year Plan</a>
-      <a href="#/don1Year" id="userLinks">1 Year Plan</a>
-      <a href="#/donWright" id="userLinks">Another Link</a>
-    </div>
-  );
+const axios = require('axios');
 
-  return (
-    <div>
-      <Navbar />
-      <ActionButtons />
-      {navLinks()}
-      <DonChart />
-    </div>
-  );
-};
+
+class DonWright extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      goals: [],
+      savings: [],
+    };
+  }
+
+  componentDidMount() {
+    const savings = [];
+    const goals = [];
+    axios.get('/savings')
+      .then((data) => {
+        data.data.map((elem) => {
+          if (elem.username === 'Don Wright') {
+            savings.push(elem);
+          }
+        });
+      })
+      .catch(err => console.log(err))
+      .finally(() => {
+        this.setState({
+          savings,
+        });
+      });
+    axios.get('/goal')
+      .then((data) => {
+        data.data.map((elem) => {
+          if (elem.username === 'Don Wright') {
+            goals.push(elem);
+          }
+        });
+      })
+      .catch(err => console.log(err))
+      .finally(() => {
+        this.setState({
+          goals,
+        });
+      });
+  }
+
+  deleteTask(e) {
+    axios.patch('/savings', { id: e })
+      .then(data => location.reload())
+      .catch(err => console.log(err));
+  }
+
+  savingsTable() {
+    const { goals, savings } = this.state;
+    return (
+      <div className="userTables">
+        <p className="h6 text-center text-success font-weight-bold">Don Savings</p>
+        <hr />
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Username</th>
+              <th scope="col">Savings Amount</th>
+              <th scope="col">Date</th>
+              <th scope="col" />
+            </tr>
+          </thead>
+          <tbody>
+            {savings.map((elem, i) => (
+              <tr key={i++}>
+                <td className={elem.isCompleted === true ? 'table-success' : ''}>{i++}</td>
+                <td className={elem.isCompleted === true ? 'table-success' : ''}>{elem.username}</td>
+                <td className={elem.isCompleted === true ? 'table-success' : ''}>{elem.amount}</td>
+                <td className={elem.isCompleted === true ? 'table-success' : ''}>{elem.Date.substring(0, 10)}</td>
+                <td className={elem.isCompleted === true ? 'table-success' : ''}>
+                  <button className="btn btn-sm btn-danger text-light" onClick={() => this.deleteTask(elem._id)}>Delete</button>
+                  {' '}
+                  {' '}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+
+  navLinks() {
+    return (
+      <div className="text-center" id="userLinkContainer">
+        <a href="#/don10Year" id="userLinks">10 Year Plan</a>
+        <a href="#/don5Year" id="userLinks">5 Year Plan</a>
+        <a href="#/don1Year" id="userLinks">1 Year Plan</a>
+      </div>
+    );
+  }
+
+
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <ActionButtons />
+        {this.navLinks()}
+        <DonChart />
+        {this.savingsTable()}
+        <hr />
+      </div>
+    );
+  }
+}
+
 export default DonWright;
